e5e5a83abc0f50119c345086eea68985a08a55ea
Diego Tabares <dtabares@gmail.com>
2016-10-19 20:20:27 -0300

    agrego tests

--- test/ValidadorTest.java
+++ test/ValidadorTest.java
@@ -1,11 +1,10 @@
-
-
 import static org.junit.Assert.*;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import application.Mapa;
+import application.Obstaculo;
 import application.Recinto;
 import application.Validador;
 
@@ -95,6 +94,128 @@ public class ValidadorTest {
 		Recinto recintoDos = new Recinto(12, 12, 5, 5);
 		
 		Assert.assertTrue(Validador.validarSiElRecintoEntraEnElMapa(mapa, recintoDos));
+	}
+	
+	@Test
+	public void creoUnObstaculoFueraDelRecinto() {
+		
+		//Creo un mapa de 200 por 200
+		Mapa mapa = new Mapa(200,200);
+		
+		//Creo un recinto de 10 por 10 y lo ubico en la posicion (30,10)
+		Recinto recinto = new Recinto(30, 10, 10, 10);
+		
+		//Agrego el recinto al mapa
+		mapa.agregarRecinto(recinto);
+		
+		Assert.assertFalse(mapa.getRecintos().isEmpty());
+		
+		//Creo un obstaculo y lo posiciono fuera del recinto
+		Obstaculo obstaculo = new Obstaculo(40, 50, 10, 10);
+		
+		Assert.assertFalse(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo));
+	}
+	
+	@Test
+	public void creoUnObstaculoFueraDelRecintoYDelMapa() {
+		
+		//Creo un mapa de 200 por 200
+		Mapa mapa = new Mapa(200,200);
+		
+		//Creo un recinto de 10 por 10 y lo ubico en la posicion (30,10)
+		Recinto recinto = new Recinto(30, 10, 10, 10);
+		
+		//Agrego el recinto al mapa
+		mapa.agregarRecinto(recinto);
+		
+		Assert.assertFalse(mapa.getRecintos().isEmpty());
+		
+		//Creo un obstaculo y lo posiciono fuera del recinto
+		Obstaculo obstaculo = new Obstaculo(-50, -30, 10, 10);
+		
+		Assert.assertFalse(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo));
+	}
+	
+	@Test
+	public void creoUnObstaculoDentroDelRecintoYDelMapa() {
+		
+		//Creo un mapa de 200 por 200
+		Mapa mapa = new Mapa(200,200);
+		
+		//Creo un recinto de 10 por 10 y lo ubico en la posicion (30,10)
+		Recinto recinto = new Recinto(30, 10, 10, 10);
+		
+		//Agrego el recinto al mapa
+		mapa.agregarRecinto(recinto);
+		
+		Assert.assertFalse(mapa.getRecintos().isEmpty());
+		
+		//Creo un obstaculo y lo posiciono dentro del recinto
+		Obstaculo obstaculo = new Obstaculo(35, 10, 2, 1);
+		
+		Assert.assertTrue(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo));
+	}
+	
+	@Test
+	public void creoDosObstaculosQueSePisan() {
+		
+		//Creo un mapa de 200 por 200
+		Mapa mapa = new Mapa(200,200);
+		
+		//Creo un recinto de 10 por 10 y lo ubico en la posicion (30,10)
+		Recinto recinto = new Recinto(30, 10, 10, 10);
+		
+		//Agrego el recinto al mapa
+		mapa.agregarRecinto(recinto);
+		
+		Assert.assertFalse(mapa.getRecintos().isEmpty());
+		
+		//Creo un obstaculo y lo posiciono dentro del recinto
+		Obstaculo obstaculo1 = new Obstaculo(35, 10, 2, 1);
+		
+		Assert.assertTrue(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo1));
+		
+		recinto.agregarObstaculo(obstaculo1);
+		
+		Assert.assertFalse(recinto.getObstaculos().isEmpty());
+		
+		//Creo un segundo obstaculo y lo posiciono dentro del recinto pisando al primero
+		Obstaculo obstaculo2 = new Obstaculo(36, 10, 2, 1);
+		
+		Assert.assertFalse(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo2));
+		
+		
+	}
+	
+	@Test
+	public void creoDosObstaculosQueNoSePisan() {
+		
+		//Creo un mapa de 200 por 200
+		Mapa mapa = new Mapa(200,200);
+		
+		//Creo un recinto de 10 por 10 y lo ubico en la posicion (30,10)
+		Recinto recinto = new Recinto(30, 10, 10, 10);
+		
+		//Agrego el recinto al mapa
+		mapa.agregarRecinto(recinto);
+		
+		Assert.assertFalse(mapa.getRecintos().isEmpty());
+		
+		//Creo un obstaculo y lo posiciono dentro del recinto
+		Obstaculo obstaculo1 = new Obstaculo(35, 10, 2, 1);
+		
+		Assert.assertTrue(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo1));
+		
+		recinto.agregarObstaculo(obstaculo1);
+		
+		Assert.assertFalse(recinto.getObstaculos().isEmpty());
+		
+		//Creo un segundo obstaculo y lo posiciono dentro del recinto pisando al primero
+		Obstaculo obstaculo2 = new Obstaculo(31, 10, 1, 1);
+		
+		Assert.assertTrue(Validador.validarSiElObstaculoEntraEnElRecinto(recinto, obstaculo2));
+		
+		
 	}
 
 }
