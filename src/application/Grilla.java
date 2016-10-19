cce8b3224cc8fcdd7d3b1d780552cca03ddf6a18
Diego Tabares <dtabares@gmail.com>
2016-10-19 20:55:58 -0300

    agrego el concepto de cuadrantes a la grilla

--- src/application/Grilla.java
+++ src/application/Grilla.java
@@ -3,23 +3,20 @@ package application;
 import java.util.Collection;
 import java.util.LinkedList;
 
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
 
 public class Grilla {
 	
+	private Collection <Cuadrante> coleccionDeCuadrantes;
 	private Collection <Shape> coleccionDeRectangulos;
-	private GridPane gridPane;
 	private float posicionX;
 	private float posicionY;
 	
 	//Constructor de la clase Grilla, crea una grilla especificando el tamaño de cada unidad, require conocer el recinto para posicionarse
 	public Grilla(Recinto recinto, int tamanio){
 		
-		coleccionDeRectangulos = new LinkedList<Shape>();	
+		this.coleccionDeCuadrantes = new LinkedList<Cuadrante>();
+		this.coleccionDeRectangulos = new LinkedList<Shape>();
 		this.posicionX = recinto.getPosicionX();
 		this.posicionY = recinto.getPosicionY();
 		
@@ -27,11 +24,8 @@ public class Grilla {
 		
 			for (int columna=0;columna<(recinto.getAncho()/tamanio);columna++){
 				
-				Rectangle rectangulo = new Rectangle((posicionX+(int)fila*tamanio),(posicionY+(int)columna*tamanio),tamanio,tamanio);
-				rectangulo.setStroke(Color.BLACK);
-				rectangulo.setFill(Color.WHITE);
-				rectangulo.setStrokeWidth(1);	
-				coleccionDeRectangulos.add(rectangulo);
+				Cuadrante cuadrante = new Cuadrante((posicionX+(int)fila*tamanio),(posicionY+(int)columna*tamanio),tamanio,tamanio);	
+				coleccionDeCuadrantes.add(cuadrante);
 				
 			}
 		}
@@ -44,8 +38,19 @@ public class Grilla {
 		
 	}
 
-	public Collection<Shape> getColleccionDeRectangulos() {
+	public Collection<Cuadrante> getColeccionDeCuadrantes() {
+		return coleccionDeCuadrantes;
+	}
+	
+	public Collection<Shape> getColeccionDeRectangulos() {
 		return coleccionDeRectangulos;
+	}
+	
+	public void prepararGrillaParaDibujo(){
+		this.coleccionDeRectangulos.clear();
+		for (Cuadrante cuadrante : this.coleccionDeCuadrantes) {
+			this.coleccionDeRectangulos.add(cuadrante.getRectangle());
+		}
 	}
 	
 }
