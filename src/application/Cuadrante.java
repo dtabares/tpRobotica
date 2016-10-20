0987264163e44c80d22ae08740cfefef6ca3d000
Diego Tabares <dtabares@gmail.com>
2016-10-19 21:12:01 -0300

    hago que cuadrante herede de formaPosicionableEnMapa

--- src/application/Cuadrante.java
+++ src/application/Cuadrante.java
@@ -3,18 +3,23 @@ package application;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Cuadrante {
+public class Cuadrante extends FormaPosicionableEnMapa{
 	
 	private boolean disponible;
 	private Rectangle rectangulo;
 	
 	
-	public Cuadrante(float posiscionX, float posicionY, float ancho, float alto){
-		this.rectangulo = new Rectangle(posiscionX,posicionY,ancho,alto);
+	public Cuadrante(float posicionX, float posicionY, float ancho, float alto){
+		this.rectangulo = new Rectangle(posicionX,posicionY,ancho,alto);
 		this.rectangulo.setStroke(Color.BLACK);
 		this.rectangulo.setFill(Color.WHITE);
 		this.rectangulo.setStrokeWidth(1);	
 		this.disponible = true;
+		this.alto = alto;
+		this.ancho = ancho;
+		this.posicionX = posicionX;
+		this.posicionY = posicionY;
+		this.calcularPosicionBordes();
 	}
 	
 	public void marcarComoDisponible(){
