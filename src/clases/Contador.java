875ba0d5b18f5a31dae67dc1a4c915c066d3b4d2
Diego Tabares <dtabares@gmail.com>
2016-11-05 20:13:51 -0300

    le agrego que implemente serializable

--- src/clases/Contador.java
+++ src/clases/Contador.java
@@ -1,7 +1,10 @@
 package clases;
 
-public class Contador {
+import java.io.Serializable;
+
+public class Contador implements Serializable{
 	
+	private static final long serialVersionUID = 2688427677898794473L;
 	private static Contador instance = null;
 	private int proximoNumeroDeCuadrante;
 	protected Contador() {
@@ -18,6 +21,10 @@ public class Contador {
 	}
 	public void setProximoNumeroDeCuadrante(int numeroDeCuadrante) {
 		this.proximoNumeroDeCuadrante = numeroDeCuadrante;
+	}
+	
+	public void resetearContador(){
+		this.proximoNumeroDeCuadrante = 0;
 	}
 	
 }
